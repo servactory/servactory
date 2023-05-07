3ac58e87d65c1bbe8f63a57bc7fc30a6830d7ffa
Anton <profox.rus@gmail.com>
2023-05-07 12:19:50 +0300

    Added tests

---
+++ examples/usual/example14.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Usual
+  class Example14 < ApplicationService::Base
+    Event = Struct.new(:id, :event_name, keyword_init: true)
+
+    input :event_name,
+          type: String,
+          inclusion: {
+            in: %w[created rejected approved]
+          }
+
+    output :event, type: Event
+
+    stage { make :create_event }
+
+    private
+
+    def create_event
+      self.event = Event.new(
+        id: "14fe213e-1b0a-4a68-bca9-ce082db0f2c6",
+        event_name: inputs.event_name
+      )
+    end
+  end
+end
