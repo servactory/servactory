fdaf4c8114a8845a8cdad8d1a9cd2f12893a4c75
Anton <profox.rus@gmail.com>
2023-05-12 22:21:28 +0300

    Added `success?` and `failure?` methods

--- lib/servactory/result.rb
+++ lib/servactory/result.rb
@@ -9,13 +9,23 @@ module Servactory
     private
 
     def prepare_for(context:, collection_of_output_arguments:)
+      prepare_outputs_with(context: context, collection_of_output_arguments: collection_of_output_arguments)
+      prepare_statuses_with(context: context)
+
+      self
+    end
+
+    def prepare_outputs_with(context:, collection_of_output_arguments:)
       collection_of_output_arguments.each do |output|
         self.class.attr_reader(:"#{output.name}")
 
         instance_variable_set(:"@#{output.name}", context.instance_variable_get(:"@#{output.name}"))
       end
+    end
 
-      self
+    def prepare_statuses_with(context:)
+      define_singleton_method(:success?) { context.errors.empty? }
+      define_singleton_method(:failure?) { !context.errors.empty? }
     end
   end
 end
