66c4943253552b4555f6994b5305b0fa2634ddb5
Anton <profox.rus@gmail.com>
2023-05-16 20:07:18 +0300

    Added tests for an example of misuse

---
+++ spec/examples/wrong/example12_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+RSpec.describe Wrong::Example12 do
+  describe ".call!" do
+    subject(:perform) { described_class.call!(**attributes) }
+
+    let(:attributes) do
+      {
+        invoice_number: invoice_number
+      }
+    end
+
+    let(:invoice_number) { "ABC-123" }
+
+    context "when the input arguments are valid" do
+      describe "but the data required for work is invalid" do
+        it "returns expected error" do
+          expect { perform }.to(
+            raise_error(
+              ApplicationService::Errors::OutputArgumentError,
+              "[Wrong::Example12] Conflict between internal and output attributes `prepared_invoice_number`"
+            )
+          )
+        end
+      end
+    end
+  end
+
+  describe ".call" do
+    subject(:perform) { described_class.call(**attributes) }
+
+    let(:attributes) do
+      {
+        invoice_number: invoice_number
+      }
+    end
+
+    let(:invoice_number) { "ABC-123" }
+
+    context "when the input arguments are valid" do
+      describe "but the data required for work is invalid" do
+        it "returns expected error" do
+          expect { perform }.to(
+            raise_error(
+              ApplicationService::Errors::OutputArgumentError,
+              "[Wrong::Example12] Conflict between internal and output attributes `prepared_invoice_number`"
+            )
+          )
+        end
+      end
+    end
+  end
+end
