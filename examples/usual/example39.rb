3a604763464fdc1ff70a087430cbb4803cf2d516
Anton <profox.rus@gmail.com>
2023-06-03 20:53:17 +0300

    Added examples

--- examples/usual/example39.rb
+++ examples/usual/example39.rb
@@ -1,25 +1,33 @@
 # frozen_string_literal: true
 
 module Usual
-  class Example37Transaction
+  class Example39Transaction
     def self.transaction(&block)
       yield if block
       # TODO: rescue Example37TransactionRollback
     end
   end
 
-  class Example37 < ApplicationService::Base
+  class Example39 < ApplicationService::Base
     output :number, type: Integer
 
+    make :assign_number_4
+
     stage do
-      wrap_in ->(methods:) { Example37Transaction.transaction { methods } }
+      wrap_in ->(methods:) { Example39Transaction.transaction { methods } }
 
       make :assign_number_5
-      make :assign_number_6
+      make :assign_number_6, position: 1
       make :assign_number_7
     end
 
+    make :assign_number_8
+
     private
+
+    def assign_number_4
+      self.number = 4
+    end
 
     def assign_number_5
       self.number = 5
@@ -31,6 +39,10 @@ module Usual
 
     def assign_number_7
       self.number = 7
+    end
+
+    def assign_number_8
+      self.number = 8
     end
   end
 end
