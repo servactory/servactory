621e8c53893f0b5455a0f6b45764fb634757fc9d
Anton <profox.rus@gmail.com>
2023-06-03 12:58:54 +0300

    Update libraries from 03.06.2023 (#57)

--- examples/usual/example34.rb
+++ examples/usual/example34.rb
@@ -8,7 +8,7 @@ module Usual
           must: {
             be_6_characters: {
               is: ->(value:) { value.all? { |id| id.size == 6 } },
-              message: lambda do |service_class_name:, input:, value:, code:|
+              message: lambda do |input:, **|
                 "Wrong IDs in `#{input.name}`"
               end
             }
