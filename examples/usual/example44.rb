4ec0ab10094b0f7be8c4568ef093877486a45a5c
Anton <profox.rus@gmail.com>
2023-06-09 20:40:11 +0300

    Added example

---
+++ examples/usual/example44.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Usual
+  class Example6 < ApplicationService::Base
+    input :ids, :as_array, type: String
+
+    output :first_id, type: String
+
+    make :assign_first_id
+
+    private
+
+    def assign_first_id
+      self.first_id = inputs.ids[0]
+    end
+  end
+end
