9d49f072b2ebdfa04241f4b6ef162e805c7a2771
Anton <profox.rus@gmail.com>
2023-06-10 17:24:43 +0300

    Added examples

--- examples/application_service/base.rb
+++ examples/application_service/base.rb
@@ -9,6 +9,24 @@ module ApplicationService
 
       failure_class ApplicationService::Errors::Failure
 
+      input_option_helpers(
+        [
+          Servactory::Inputs::OptionHelper.new(
+            name: :must_be_6_characters,
+            equivalent: {
+              must: {
+                be_6_characters: {
+                  is: ->(value:) { value.all? { |id| id.size == 6 } },
+                  message: lambda do |input:, **|
+                    "Wrong IDs in `#{input.name}`"
+                  end
+                }
+              }
+            }
+          )
+        ]
+      )
+
       method_shortcuts %i[assign]
     end
   end
