0bb78df8b90507dd5fcf654bff2428e16d7ae9d7
Anton <profox.rus@gmail.com>
2023-06-11 12:35:24 +0300

    Added example

---
+++ examples/usual/example47.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Usual
+  class Example47 < ApplicationService::Base
+    Money = Struct.new(:cents, :currency, keyword_init: true) do
+      def +(other)
+        self.class.new(cents: cents + other.cents, currency: currency)
+      end
+    end
+
+    BONUS = Money.new(cents: 1_000_00, currency: :USD)
+
+    private_constant :BONUS
+
+    ############################################################################
+
+    input :balance_cents,
+          as: :balance,
+          type: Integer,
+          prepare: ->(value:) { Money.new(cents: value, currency: :USD) }
+
+    output :balance_with_bonus, type: Money
+
+    make :assign_balance_with_bonus
+
+    private
+
+    def assign_balance_with_bonus
+      self.balance_with_bonus = inputs.balance + BONUS
+    end
+  end
+end
