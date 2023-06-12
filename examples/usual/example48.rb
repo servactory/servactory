794427f1474cef803b52f82a1cddfe34a94d2631
Anton <profox.rus@gmail.com>
2023-06-12 16:13:46 +0300

    Add `only_if` method for `stage` (#68)

---
+++ examples/usual/example48.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Usual
+  class Example48 < ApplicationService::Base
+    output :number, type: Integer
+
+    stage do
+      only_if true
+
+      make :assign_number_6
+    end
+
+    stage do
+      only_if false
+
+      make :assign_number_7
+    end
+
+    private
+
+    def assign_number_6
+      self.number = 6
+    end
+
+    def assign_number_7
+      self.number = 7
+    end
+  end
+end
