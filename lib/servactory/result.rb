538c66cd33bd263260f400bf99b74172393f8f76
Anton <profox.rus@gmail.com>
2023-06-24 12:23:32 +0300

    Improved result

--- lib/servactory/result.rb
+++ lib/servactory/result.rb
@@ -3,39 +3,61 @@
 module Servactory
   class Result
     def self.success_for(...)
-      new.send(:success_for, ...)
+      new(...).send(:as_success)
     end
 
     def self.failure_for(...)
-      new.send(:failure_for, ...)
+      new(...).send(:as_failure)
+    end
+
+    def initialize(context: nil, collection_of_outputs: nil, exception: nil)
+      @context = context
+      @collection_of_outputs = collection_of_outputs
+      @exception = exception
+    end
+
+    def method_missing(name, *args, &block)
+      output = @collection_of_outputs&.find_by(name: name)
+
+      return super if output.nil?
+
+      @context.instance_variable_get(:"@#{output.name}")
+    end
+
+    def respond_to_missing?(name, *)
+      @collection_of_outputs&.names&.include?(name) || super
+    end
+
+    def inspect
+      "#<#{self.class.name} #{draw_result}>"
     end
 
     private
 
-    def success_for(context:, collection_of_outputs:)
-      prepare_outputs_with(context: context, collection_of_outputs: collection_of_outputs)
+    def draw_result
+      string = ""
 
+      @collection_of_outputs&.each do |output|
+        string += "@#{output.name}=#{@context.instance_variable_get(:"@#{output.name}").inspect}"
+      end
+
+      string
+    end
+
+    def as_success
       define_singleton_method(:success?) { true }
       define_singleton_method(:failure?) { false }
 
       self
     end
 
-    def failure_for(exception:)
-      define_singleton_method(:error) { exception }
+    def as_failure
+      define_singleton_method(:error) { @exception }
 
       define_singleton_method(:success?) { false }
       define_singleton_method(:failure?) { true }
 
       self
-    end
-
-    def prepare_outputs_with(context:, collection_of_outputs:)
-      collection_of_outputs.each do |output|
-        self.class.attr_reader(:"#{output.name}")
-
-        instance_variable_set(:"@#{output.name}", context.instance_variable_get(:"@#{output.name}"))
-      end
     end
   end
 end
