67d7ef9d95a3d237486d00a5a6dafbf91888f767
Anton <profox.rus@gmail.com>
2023-06-29 16:34:19 +0300

    Harmlessly getting data from internals and outputs (#93)

---
+++ examples/wrong/example23.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Wrong
+  class Example23 < ApplicationService::Base
+    input :invoice_number, type: String
+
+    internal :prepared_invoice_number, type: String
+
+    output :invoice_number, type: Integer
+
+    make :assign_invoice_number
+
+    private
+
+    def assign_invoice_number
+      outputs.invoice_number = internals.prepared_invoice_number
+    end
+  end
+end
