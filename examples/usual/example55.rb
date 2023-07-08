c2915dd7b1b22531df067fee1b1defa005072fc8
Anton <profox.rus@gmail.com>
2023-07-08 11:54:48 +0300

    Added example

---
+++ examples/usual/example55.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Usual
+  class Example55 < ApplicationService::Base
+    configuration do
+      input_option_helpers(
+        Servactory::Inputs::OptionHelpers::Types.use
+      )
+    end
+
+    input :first_name, :string
+    input :middle_name, :string?
+    input :last_name, :string
+
+    output :full_name, type: String
+
+    make :assign_full_name
+
+    private
+
+    def assign_full_name
+      outputs.full_name = [
+        inputs.first_name,
+        inputs.middle_name,
+        inputs.last_name
+      ].compact.join(" ")
+    end
+  end
+end
