a5636596766a58e701096e8f9128bb87eafb2695
Anton <profox.rus@gmail.com>
2023-07-20 14:09:05 +0300

    Improved `Servactory::TestKit::Result`

--- lib/servactory/test_kit/result.rb
+++ lib/servactory/test_kit/result.rb
@@ -3,10 +3,24 @@
 module Servactory
   module TestKit
     class Result
-      def initialize(**attributes)
+      def self.as_success(attributes = {})
+        Servactory::Result.success_for(context: new(attributes))
+      end
+
+      def self.as_failure(exception: nil)
+        Servactory::Result.failure_for(exception: exception)
+      end
+
+      def initialize(attributes = {})
         attributes.each_pair do |method_name, method_return|
-          define_singleton_method(method_name) { method_return }
+          service_storage[:outputs].merge!({ method_name => method_return })
         end
+      end
+
+      private
+
+      def service_storage
+        @service_storage ||= { outputs: {} }
       end
     end
   end
