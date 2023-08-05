e47f030bd2890e2461201e8f6bfc065ddb5c81aa
Anton <profox.rus@gmail.com>
2023-08-05 12:50:38 +0300

    Added example

---
+++ examples/usual/example58.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Usual
+  class Example58 < ApplicationService::Base
+    input :first_name, type: String
+    input :middle_name, type: String, required: false
+    input :last_name, type: String
+
+    output :full_name, type: String
+
+    make :assign_full_name
+
+    private
+
+    def assign_full_name
+      outputs.full_name = inputs.only(:first_name, :middle_name, :last_name).values.compact.join(" ")
+    end
+  end
+end
