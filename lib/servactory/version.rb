be5f75262cd59167555d036f61d0824bdbd8f187
Anton <profox.rus@gmail.com>
2023-10-09 00:45:53 +0700

    Added pre-release part to version

--- lib/servactory/version.rb
+++ lib/servactory/version.rb
@@ -5,7 +5,8 @@ module Servactory
     MAJOR = 1
     MINOR = 9
     PATCH = 5
+    PRE = nil
 
-    STRING = [MAJOR, MINOR, PATCH].join(".")
+    STRING = [MAJOR, MINOR, PATCH, PRE].compact.join(".")
   end
 end
