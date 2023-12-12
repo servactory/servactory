dd4ce0db5f1b56884046671c10e30e879109c928
Anton <profox.rus@gmail.com>
2023-12-12 20:35:26 +0700

    Add `fail_result!` method (#139)

---
+++ spec/examples/wrong/example35_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+RSpec.describe Wrong::Example35 do
+  describe ".call!" do
+    subject(:perform) { described_class.call! }
+
+    include_examples "check class info",
+                     inputs: %i[],
+                     internals: %i[],
+                     outputs: %i[]
+
+    context "when the input arguments are valid" do
+      describe "but the data required for work is invalid" do
+        it "returns expected error" do
+          expect { perform }.to(
+            raise_error(
+              ApplicationService::Errors::Failure,
+              "Some error"
+            )
+          )
+        end
+      end
+    end
+  end
+
+  describe ".call" do
+    subject(:perform) { described_class.call }
+
+    include_examples "check class info",
+                     inputs: %i[],
+                     internals: %i[],
+                     outputs: %i[]
+
+    context "when the input arguments are valid" do
+      describe "but the data required for work is invalid" do
+        it "returns the expected value in `errors`", :aggregate_failures do
+          result = perform
+
+          expect(result.error).to be_a(ApplicationService::Errors::Failure)
+          expect(result.error).to an_object_having_attributes(
+            message: "Some error",
+            meta: {
+              some: :data
+            }
+          )
+        end
+      end
+    end
+  end
+end
