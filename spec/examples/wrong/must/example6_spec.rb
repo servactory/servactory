64d4c26fd178539df77d15ba78f6ae28e463765d
Anton <profox.rus@gmail.com>
2024-01-11 14:31:49 +0700

    Added examples

---
+++ spec/examples/wrong/must/example6_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+RSpec.describe Wrong::Must::Example6 do
+  describe ".call!" do
+    subject(:perform) { described_class.call! }
+
+    include_examples "check class info",
+                     inputs: %i[],
+                     internals: %i[],
+                     outputs: %i[invoice_numbers first_invoice_number]
+
+    context "when the input arguments are valid" do
+      describe "but the data required for work is invalid" do
+        it "returns expected error" do
+          expect { perform }.to(
+            raise_error(
+              ApplicationService::Errors::OutputError,
+              "Wrong IDs in `invoice_numbers`"
+            )
+          )
+        end
+      end
+    end
+  end
+
+  describe ".call" do
+    subject(:perform) { described_class.call }
+
+    include_examples "check class info",
+                     inputs: %i[],
+                     internals: %i[],
+                     outputs: %i[invoice_numbers first_invoice_number]
+
+    context "when the input arguments are valid" do
+      describe "but the data required for work is invalid" do
+        it "returns expected error" do
+          expect { perform }.to(
+            raise_error(
+              ApplicationService::Errors::OutputError,
+              "Wrong IDs in `invoice_numbers`"
+            )
+          )
+        end
+      end
+    end
+  end
+end
