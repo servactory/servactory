e60f3ea201891865e2251eb53e632f4aef3f5773
Anton <profox.rus@gmail.com>
2024-01-14 16:18:59 +0700

    Added `fail_output!`

--- lib/servactory/errors/output_error.rb
+++ lib/servactory/errors/output_error.rb
@@ -3,10 +3,12 @@
 module Servactory
   module Errors
     class OutputError < Base
-      attr_reader :message
+      attr_reader :message,
+                  :output_name
 
-      def initialize(message:)
+      def initialize(message:, output_name: nil)
         @message = message
+        @output_name = output_name&.to_sym
 
         super(message)
       end
