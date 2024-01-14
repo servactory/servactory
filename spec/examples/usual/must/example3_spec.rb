f575595e941672006881c24198272c657e61224b
Anton <profox.rus@gmail.com>
2024-01-14 12:31:50 +0700

    Add support for `must` option for `output` (#149)

---
+++ spec/examples/usual/must/example3_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+RSpec.describe Usual::Must::Example3 do
+  describe ".call!" do
+    subject(:perform) { described_class.call! }
+
+    include_examples "check class info",
+                     inputs: %i[],
+                     internals: %i[],
+                     outputs: %i[invoice_numbers first_invoice_number]
+
+    context "when the input arguments are valid" do
+      describe "and the data required for work is also valid" do
+        include_examples "success result class"
+
+        it "returns the expected value in `first_id`" do
+          result = perform
+
+          expect(result.first_invoice_number).to eq("7650AE")
+        end
+      end
+    end
+  end
+
+  describe ".call" do
+    subject(:perform) { described_class.call }
+
+    include_examples "check class info",
+                     inputs: %i[],
+                     internals: %i[],
+                     outputs: %i[invoice_numbers first_invoice_number]
+
+    context "when the input arguments are valid" do
+      describe "and the data required for work is also valid" do
+        include_examples "success result class"
+
+        it "returns the expected value in `first_id`" do
+          result = perform
+
+          expect(result.first_invoice_number).to eq("7650AE")
+        end
+      end
+    end
+  end
+end
