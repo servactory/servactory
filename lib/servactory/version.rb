b4e5cfe7ec4390c895a2fab2d33efa63d7dfcb3f
Anton <profox.rus@gmail.com>
2024-02-19 20:03:32 +0700

    v2.2.0.rc1

--- lib/servactory/version.rb
+++ lib/servactory/version.rb
@@ -3,9 +3,9 @@
 module Servactory
   module VERSION
     MAJOR = 2
-    MINOR = 1
-    PATCH = 1
-    PRE = nil
+    MINOR = 2
+    PATCH = 0
+    PRE = "rc1"
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join(".")
   end
