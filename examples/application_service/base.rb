0f9568120b29f699f8c2a89d544ba34829ffd4aa
Anton <profox.rus@gmail.com>
2024-03-04 20:45:46 +0700

    Updated example

--- examples/application_service/base.rb
+++ examples/application_service/base.rb
@@ -56,13 +56,12 @@ module ApplicationService
               {
                 must: {
                   be_less_than_or_equal_to: {
-                    is: ->(value:) { value >= received_value },
+                    is: ->(value:) { value <= received_value },
                     message: lambda do |service_class_name:, input:, value:, **|
                       "[#{service_class_name}] #{input.system_name.to_s.titleize} attribute `#{input.name}` " \
                         "received value `#{value}`, which is greater than `#{received_value}`"
                     end
                   }
-                }
                 }
               }
             end
