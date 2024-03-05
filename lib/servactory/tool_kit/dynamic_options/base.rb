2787385669292fe485aab6af062810a398d2762a
Anton <profox.rus@gmail.com>
2024-03-05 19:41:17 +0700

    Improved class structure

--- lib/servactory/tool_kit/dynamic_options/base.rb
+++ lib/servactory/tool_kit/dynamic_options/base.rb
@@ -27,8 +27,26 @@ module Servactory
           end
         end
 
-        def must_content_with(_received_value)
-          raise "Need to implement `must_content_with(received_value)` method"
+        def must_content_with(received_value) # rubocop:disable Metrics/MethodLength
+          {
+            is: ->(value:) { condition_with(value, received_value) },
+            message: lambda do |**attributes|
+              message_for(
+                **attributes,
+                attribute_name: attributes[:input].system_name.to_s.titleize,
+                input_name: attributes[:input].name,
+                received_value: received_value
+              )
+            end
+          }
+        end
+
+        def condition_with(_value, _received_value)
+          raise "Need to implement `condition_with(value, received_value)` method"
+        end
+
+        def message_for(**)
+          raise "Need to implement `message_for(**attributes)` method"
         end
       end
     end
--- lib/servactory/tool_kit/dynamic_options/min.rb
+++ lib/servactory/tool_kit/dynamic_options/min.rb
@@ -8,22 +8,11 @@ module Servactory
           new(:min).setup(:be_greater_than_or_equal_to)
         end
 
-        def must_content_with(received_value) # rubocop:disable Metrics/MethodLength
-          {
-            is: ->(value:) { value >= received_value },
-            message: lambda do |service_class_name:, input:, value:, **|
-              message_for(
-                service_class_name: service_class_name,
-                attribute_name: input.system_name.to_s.titleize,
-                input_name: input.name,
-                value: value,
-                received_value: received_value
-              )
-            end
-          }
+        def condition_with(value, received_value)
+          value >= received_value
         end
 
-        def message_for(service_class_name:, attribute_name:, input_name:, value:, received_value:)
+        def message_for(service_class_name:, attribute_name:, input_name:, value:, received_value:, **)
           "[#{service_class_name}] #{attribute_name} attribute `#{input_name}` " \
             "received value `#{value}`, which is less than `#{received_value}`"
         end
