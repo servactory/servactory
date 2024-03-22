6b6ad79cd2dd520a10226c7219dc7ea61a9a20b4
Anton <profox.rus@gmail.com>
2024-03-23 00:49:39 +0700

    Fixed mock call

--- lib/servactory/context/callable.rb
+++ lib/servactory/context/callable.rb
@@ -21,7 +21,20 @@ module Servactory
       end
 
       def call(arguments = {})
-        call!(arguments)
+        context = send(:new)
+
+        context.send(
+          :_call!,
+          incoming_arguments: arguments.symbolize_keys,
+          collection_of_inputs: collection_of_inputs,
+          collection_of_internals: collection_of_internals,
+          collection_of_outputs: collection_of_outputs,
+          collection_of_stages: collection_of_stages
+        )
+
+        Servactory::Result.success_for(context: context)
+      rescue config.success_class => e
+        Servactory::Result.success_for(context: e.context)
       rescue config.failure_class => e
         Servactory::Result.failure_for(exception: e)
       end
