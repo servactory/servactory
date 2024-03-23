d5674d076d21d79d0484e122b3d6f2b489134ea5
Anton <profox.rus@gmail.com>
2024-03-23 13:26:43 +0700

    Improvements

--- lib/servactory/context/callable.rb
+++ lib/servactory/context/callable.rb
@@ -3,38 +3,43 @@
 module Servactory
   module Context
     module Callable
-      def call!(arguments = {}) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
-        unless Object.const_defined?("#{name}::Result")
-          class_eval <<-RUBY, __FILE__, __LINE__ + 1
-            Result = Class.new(Servactory::Result)
-          RUBY
-        end
+      def call!(arguments = {})
+        prepare_result_class
 
         context = send(:new)
 
-        context.send(
-          :_call!,
-          incoming_arguments: arguments.symbolize_keys,
-          collection_of_inputs: collection_of_inputs,
-          collection_of_internals: collection_of_internals,
-          collection_of_outputs: collection_of_outputs,
-          collection_of_stages: collection_of_stages
-        )
+        _call!(context, **arguments)
 
-        context.class::Result.success_for(context: context)
+        self::Result.success_for(context: context)
       rescue config.success_class => e
-        context.class::Result.success_for(context: e.context)
+        self::Result.success_for(context: e.context)
       end
 
-      def call(arguments = {}) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
-        unless Object.const_defined?("#{name}::Result")
-          class_eval <<-RUBY, __FILE__, __LINE__ + 1
-            Result = Class.new(Servactory::Result)
-          RUBY
-        end
+      def call(arguments = {})
+        prepare_result_class
 
         context = send(:new)
 
+        _call!(context, **arguments)
+
+        self::Result.success_for(context: context)
+      rescue config.success_class => e
+        self::Result.success_for(context: e.context)
+      rescue config.failure_class => e
+        self::Result.failure_for(exception: e)
+      end
+
+      private
+
+      def prepare_result_class
+        return if Object.const_defined?("#{name}::Result")
+
+        class_eval <<-RUBY, __FILE__, __LINE__ + 1
+          Result = Class.new(Servactory::Result)
+        RUBY
+      end
+
+      def _call!(context, **arguments)
         context.send(
           :_call!,
           incoming_arguments: arguments.symbolize_keys,
@@ -43,12 +48,6 @@ module Servactory
           collection_of_outputs: collection_of_outputs,
           collection_of_stages: collection_of_stages
         )
-
-        context.class::Result.success_for(context: context)
-      rescue config.success_class => e
-        context.class::Result.success_for(context: e.context)
-      rescue config.failure_class => e
-        context.class::Result.failure_for(exception: e)
       end
     end
   end
