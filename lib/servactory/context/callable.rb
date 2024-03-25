89305fa0c01107a05bee884f2bd05c9617f1fb85
Anton <profox.rus@gmail.com>
2024-03-25 20:20:26 +0700

    Improve name of Result class by adding name of service class (#192)

--- lib/servactory/context/callable.rb
+++ lib/servactory/context/callable.rb
@@ -3,26 +3,43 @@
 module Servactory
   module Context
     module Callable
-      def call!(arguments = {}) # rubocop:disable Metrics/MethodLength
+      def call!(arguments = {})
+        prepare_result_class
+
         context = send(:new)
 
-        context.send(
-          :_call!,
-          incoming_arguments: arguments.symbolize_keys,
-          collection_of_inputs: collection_of_inputs,
-          collection_of_internals: collection_of_internals,
-          collection_of_outputs: collection_of_outputs,
-          collection_of_stages: collection_of_stages
-        )
+        _call!(context, **arguments)
 
-        Servactory::Result.success_for(context: context)
+        self::Result.success_for(context: context)
       rescue config.success_class => e
-        Servactory::Result.success_for(context: e.context)
+        self::Result.success_for(context: e.context)
       end
 
       def call(arguments = {})
+        prepare_result_class
+
         context = send(:new)
 
+        _call!(context, **arguments)
+
+        self::Result.success_for(context: context)
+      rescue config.success_class => e
+        self::Result.success_for(context: e.context)
+      rescue config.failure_class => e
+        self::Result.failure_for(exception: e)
+      end
+
+      private
+
+      def prepare_result_class
+        return if Object.const_defined?("#{name}::Result")
+
+        class_eval <<-RUBY, __FILE__, __LINE__ + 1
+          Result = Class.new(Servactory::Result)
+        RUBY
+      end
+
+      def _call!(context, **arguments)
         context.send(
           :_call!,
           incoming_arguments: arguments.symbolize_keys,
@@ -31,12 +48,6 @@ module Servactory
           collection_of_outputs: collection_of_outputs,
           collection_of_stages: collection_of_stages
         )
-
-        Servactory::Result.success_for(context: context)
-      rescue config.success_class => e
-        Servactory::Result.success_for(context: e.context)
-      rescue config.failure_class => e
-        Servactory::Result.failure_for(exception: e)
       end
     end
   end
--- spec/support/shared_examples/failure_result_class.rb
+++ spec/support/shared_examples/failure_result_class.rb
@@ -4,10 +4,14 @@ RSpec.shared_examples "failure result class" do
   it "returns failure result class", :aggregate_failures do
     result = perform
 
+    expect(result).to be_an_instance_of(described_class::Result)
     expect(result).to be_a(Servactory::Result)
+    expect(described_class::Result < Servactory::Result).to be(true)
+
     expect(result.success?).to be(false)
     expect(result.failure?).to be(true)
     expect(result.failure?(:all)).to be(true)
     expect(result.failure?(:base)).to be(true)
+    expect(result.failure?(:non_existent)).to be(false)
   end
 end
