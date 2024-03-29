6310590324fc3f1d300a9f2efc471ca014616521
Anton <profox.rus@gmail.com>
2024-03-29 23:54:22 +0700

    Added output data transfer to `on_failure`

--- spec/examples/usual/on_failure/example1_spec.rb
+++ spec/examples/usual/on_failure/example1_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Usual::OnFailure::Example1 do
     include_examples "check class info",
                      inputs: %i[],
                      internals: %i[],
-                     outputs: %i[]
+                     outputs: %i[some_value]
 
     context "when the input arguments are valid" do
       describe "but the data required for work is invalid" do
@@ -31,7 +31,7 @@ RSpec.describe Usual::OnFailure::Example1 do
     include_examples "check class info",
                      inputs: %i[],
                      internals: %i[],
-                     outputs: %i[]
+                     outputs: %i[some_value]
 
     context "when the input arguments are valid" do
       describe "but the data required for work is invalid" do
@@ -55,12 +55,12 @@ RSpec.describe Usual::OnFailure::Example1 do
 
           expect do
             result.on_success { performed_methods.push(:call_method_on_success) }
-                  .on_failure(:all) { |**| performed_methods.push(:call_method_on_failure_all) }
+                  .on_failure(:all) { |outputs:, **| performed_methods.push(outputs.some_value) }
                   .on_failure(:base) { |**| performed_methods.push(:call_method_on_failure_base) }
                   .on_failure(:validation) { |**| performed_methods.push(:call_method_on_failure_validation) }
           end.to(
             change { performed_methods }.from([]).to(
-              %i[call_method_on_failure_all call_method_on_failure_validation]
+              %i[some_value call_method_on_failure_validation]
             )
           )
         end
