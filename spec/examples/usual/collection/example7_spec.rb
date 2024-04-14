ea80bbca8f20fc3ae88676fe3498eb704e9ae28a
Anton <profox.rus@gmail.com>
2024-04-14 20:04:27 +0700

    Updated specs

--- spec/examples/usual/collection/example7_spec.rb
+++ spec/examples/usual/collection/example7_spec.rb
@@ -28,23 +28,24 @@ RSpec.describe Usual::Collection::Example7, type: :service do
       describe "and the data required for work is also valid" do
         include_examples "success result class"
 
-        it "returns expected values", :aggregate_failures do
-          result = perform
+        it { expect(perform).to have_output(:ids?).with(true) }
 
-          expect(result.ids?).to be(true)
-          expect(result.ids).to(
-            match_array(
-              Set[
-                "6e6ff7d9-6980-4c98-8fd8-ca615ccebab3",
-                "bdd30bb6-c6ab-448d-8302-7018de07b9a4",
-                "e864b5e7-e515-4d5e-9a7e-7da440323390",
-                "b0f7c462-86a4-4e5b-8d56-5dcfcabe0f81"
-              ]
-            )
+        it do
+          expect(perform).to(
+            have_output(:ids)
+              .with(
+                Set[
+                  "6e6ff7d9-6980-4c98-8fd8-ca615ccebab3",
+                  "bdd30bb6-c6ab-448d-8302-7018de07b9a4",
+                  "e864b5e7-e515-4d5e-9a7e-7da440323390",
+                  "b0f7c462-86a4-4e5b-8d56-5dcfcabe0f81"
+                ]
+              )
           )
-          expect(result.first_id?).to be(true)
-          expect(result.first_id).to eq("6e6ff7d9-6980-4c98-8fd8-ca615ccebab3")
         end
+
+        it { expect(perform).to have_output(:first_id?).with(true) }
+        it { expect(perform).to have_output(:first_id).with("6e6ff7d9-6980-4c98-8fd8-ca615ccebab3") }
       end
 
       describe "but the data required for work is invalid" do
@@ -139,23 +140,24 @@ RSpec.describe Usual::Collection::Example7, type: :service do
       describe "and the data required for work is also valid" do
         include_examples "success result class"
 
-        it "returns expected values", :aggregate_failures do
-          result = perform
+        it { expect(perform).to have_output(:ids?).with(true) }
 
-          expect(result.ids?).to be(true)
-          expect(result.ids).to(
-            match_array(
-              Set[
-                "6e6ff7d9-6980-4c98-8fd8-ca615ccebab3",
-                "bdd30bb6-c6ab-448d-8302-7018de07b9a4",
-                "e864b5e7-e515-4d5e-9a7e-7da440323390",
-                "b0f7c462-86a4-4e5b-8d56-5dcfcabe0f81"
-              ]
-            )
+        it do
+          expect(perform).to(
+            have_output(:ids)
+              .with(
+                Set[
+                  "6e6ff7d9-6980-4c98-8fd8-ca615ccebab3",
+                  "bdd30bb6-c6ab-448d-8302-7018de07b9a4",
+                  "e864b5e7-e515-4d5e-9a7e-7da440323390",
+                  "b0f7c462-86a4-4e5b-8d56-5dcfcabe0f81"
+                ]
+              )
           )
-          expect(result.first_id?).to be(true)
-          expect(result.first_id).to eq("6e6ff7d9-6980-4c98-8fd8-ca615ccebab3")
         end
+
+        it { expect(perform).to have_output(:first_id?).with(true) }
+        it { expect(perform).to have_output(:first_id).with("6e6ff7d9-6980-4c98-8fd8-ca615ccebab3") }
       end
 
       describe "but the data required for work is invalid" do
