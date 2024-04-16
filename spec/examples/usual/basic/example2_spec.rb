759661168f920a351fda2932f566cd3fcc4f1479
Anton <profox.rus@gmail.com>
2024-04-16 23:07:48 +0700

    Deleted unused code

--- spec/examples/usual/basic/example2_spec.rb
+++ spec/examples/usual/basic/example2_spec.rb
@@ -18,10 +18,6 @@ RSpec.describe Usual::Basic::Example2, type: :service do
       it { expect { perform }.to have_input(:first_name).valid_with(attributes).type(String).required }
       it { expect { perform }.to have_input(:middle_name).valid_with(attributes).type(String).optional }
       it { expect { perform }.to have_input(:last_name).valid_with(attributes).type(String).required }
-
-      # describe "negatively" do
-      #   it { expect { perform }.to validate_negative_input(:first_name).with(attributes) }
-      # end
     end
 
     describe "internals" do
