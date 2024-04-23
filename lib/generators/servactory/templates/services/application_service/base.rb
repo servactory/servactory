dae684a9597c65a6a2d6164aca72f44f5155252a
Anton <profox.rus@gmail.com>
2024-04-23 20:15:42 +0700

    Add generators (#201)

---
+++ lib/generators/servactory/templates/services/application_service/base.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+module ApplicationService
+  class Base
+    include Servactory::DSL
+
+    # More information: https://servactory.com/guide/extensions
+    # include Servactory::DSL.with_extensions(
+    #   ApplicationService::Extensions::YourExtension::DSL
+    # )
+
+    fail_on! ActiveRecord::RecordInvalid
+
+    # More information: https://servactory.com/guide/configuration
+    configuration do
+      input_exception_class ApplicationService::Exceptions::Input
+      internal_exception_class ApplicationService::Exceptions::Internal
+      output_exception_class ApplicationService::Exceptions::Output
+
+      failure_class ApplicationService::Exceptions::Failure
+
+      result_class ApplicationService::Result
+
+      # input_option_helpers(
+      #   [
+      #     Servactory::ToolKit::DynamicOptions::Format.use,
+      #     Servactory::ToolKit::DynamicOptions::Min.use,
+      #     Servactory::ToolKit::DynamicOptions::Max.use,
+      #     ApplicationService::DynamicOptions::CustomEq.use
+      #   ]
+      # )
+
+      # internal_option_helpers(
+      #   [
+      #     Servactory::ToolKit::DynamicOptions::Format.use,
+      #     Servactory::ToolKit::DynamicOptions::Min.use,
+      #     Servactory::ToolKit::DynamicOptions::Max.use,
+      #     ApplicationService::DynamicOptions::CustomEq.use
+      #   ]
+      # )
+
+      # output_option_helpers(
+      #   [
+      #     Servactory::ToolKit::DynamicOptions::Format.use,
+      #     Servactory::ToolKit::DynamicOptions::Min.use,
+      #     Servactory::ToolKit::DynamicOptions::Max.use,
+      #     ApplicationService::DynamicOptions::CustomEq.use
+      #   ]
+      # )
+
+      # collection_mode_class_names [ActiveRecord::Relation]
+
+      # hash_mode_class_names [CustomHash]
+
+      # action_shortcuts %i[assign build create save]
+
+      # action_aliases %i[do_it!]
+    end
+  end
+end
