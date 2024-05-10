438b1702320475066bcf1960a4931766f58ec79f
Anton <profox.rus@gmail.com>
2024-05-10 19:34:19 +0700

    Add i18n_root_key to configuration

--- lib/servactory/configuration/dsl.rb
+++ lib/servactory/configuration/dsl.rb
@@ -28,6 +28,8 @@ module Servactory
           child.config.action_shortcuts = config.action_shortcuts
           child.config.action_rescue_handlers = config.action_rescue_handlers
 
+          child.config.i18n_root_key = config.i18n_root_key
+
           child.config.predicate_methods_enabled = config.predicate_methods_enabled
         end
 
