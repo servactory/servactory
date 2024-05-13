a43850d275774c96cca817cefbc647010f1f2915
Anton <profox.rus@gmail.com>
2024-05-13 20:40:16 +0700

    Add PredicateMethodsEnabled example service and related tests
    This commit includes the creation of a new service under Usual::PredicateMethodsEnabled named Example1. This service has three input attributes (first_name, middle_name, last_name) and one output (full_name). A comprehensive set of tests has also been added verifying both valid and invalid input arguments and their corresponding output.

---
+++ examples/usual/predicate_methods_enabled/example1.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Usual
+  module PredicateMethodsEnabled
+    class Example1 < ApplicationService::Base
+      configuration do
+        predicate_methods_enabled false
+      end
+
+      input :first_name, type: String
+      input :middle_name, type: String, required: false
+      input :last_name, type: String
+
+      output :full_name, type: String
+
+      make :assign_full_name
+
+      private
+
+      def assign_full_name
+        outputs.full_name = [
+          inputs.first_name,
+          inputs.middle_name,
+          inputs.last_name
+        ].compact.join(" ")
+      end
+    end
+  end
+end
