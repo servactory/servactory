c9edc0c7659460e6f8ee0150bfac7dc509e54411
Anton <profox.rus@gmail.com>
2024-05-19 21:15:55 +0700

    Add optional password examples and associated specs
    This commit introduces examples related to optional passwords handling in the `Usual::DynamicOptions::Format::Password::Optional` namespace. Corresponding rspec tests have also been written to ensure functionality.

---
+++ spec/examples/usual/dynamic_options/format/password/optional/example2_spec.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+RSpec.describe Usual::DynamicOptions::Format::Password::Optional::Example2, type: :service do
+  describe ".call!" do
+    subject(:perform) { described_class.call!(**attributes) }
+
+    let(:attributes) do
+      {
+        password: password
+      }
+    end
+
+    let(:password) { nil }
+
+    include_examples "check class info",
+                     inputs: %i[password],
+                     internals: %i[password],
+                     outputs: %i[password]
+
+    context "when the input arguments are valid" do
+      describe "and the data required for work is also valid" do
+        it { expect(perform).to have_output(:password?).with(false) }
+        it { expect(perform).to have_output(:password).with(nil) }
+      end
+
+      describe "but the data required for work is invalid" do
+        describe "because the format is not suitable for `password`" do
+          let(:password) { "my-best-password" }
+
+          it "returns expected error" do
+            expect { perform }.to(
+              raise_error(
+                ApplicationService::Exceptions::Internal,
+                "[Usual::DynamicOptions::Format::Password::Optional::Example2] " \
+                "Internal attribute `password` does not match `password` format"
+              )
+            )
+          end
+        end
+      end
+    end
+
+    context "when the input arguments are invalid" do
+      it { expect { perform }.to have_input(:password).valid_with(attributes).type(String).optional }
+    end
+  end
+
+  describe ".call" do
+    subject(:perform) { described_class.call(**attributes) }
+
+    let(:attributes) do
+      {
+        password: password
+      }
+    end
+
+    let(:password) { nil }
+
+    include_examples "check class info",
+                     inputs: %i[password],
+                     internals: %i[password],
+                     outputs: %i[password]
+
+    context "when the input arguments are valid" do
+      describe "and the data required for work is also valid" do
+        it { expect(perform).to have_output(:password?).with(false) }
+        it { expect(perform).to have_output(:password).with(nil) }
+      end
+
+      describe "but the data required for work is invalid" do
+        describe "because the format is not suitable for `password`" do
+          let(:password) { "my-best-password" }
+
+          it "returns expected error" do
+            expect { perform }.to(
+              raise_error(
+                ApplicationService::Exceptions::Internal,
+                "[Usual::DynamicOptions::Format::Password::Optional::Example2] " \
+                "Internal attribute `password` does not match `password` format"
+              )
+            )
+          end
+        end
+      end
+    end
+
+    context "when the input arguments are invalid" do
+      it { expect { perform }.to have_input(:password).valid_with(attributes).type(String).optional }
+    end
+  end
+end
