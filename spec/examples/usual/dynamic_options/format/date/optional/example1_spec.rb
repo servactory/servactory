5a5698fcecbdb6b0fdad54a08a848d647c65f972
Anton <profox.rus@gmail.com>
2024-05-19 16:57:37 +0700

    Add date format validation for optional fields in DynamicOptions
    This update introduces new classes under DynamicOptions module to handle validation of optional date fields. Through these classes, dates are effectively validated and converted into the required format, ensuring data integrity. The corresponding spec files for these classes were created, outlining a comprehensive list of tests for various valid and invalid scenarios.

---
+++ spec/examples/usual/dynamic_options/format/date/optional/example1_spec.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+RSpec.describe Usual::DynamicOptions::Format::Date::Optional::Example1, type: :service do
+  describe ".call!" do
+    subject(:perform) { described_class.call!(**attributes) }
+
+    let(:attributes) do
+      {
+        started_on: started_on
+      }
+    end
+
+    let(:started_on) { nil }
+
+    include_examples "check class info",
+                     inputs: %i[started_on],
+                     internals: %i[],
+                     outputs: %i[started_on]
+
+    context "when the input arguments are valid" do
+      describe "and the data required for work is also valid" do
+        it { expect(perform).to have_output(:started_on?).with(false) }
+        it { expect(perform).to have_output(:started_on).with(nil) }
+      end
+
+      describe "but the data required for work is invalid" do
+        describe "because the format is not suitable for `date`" do
+          let(:started_on) { "2023-14-14" }
+
+          it "returns expected error" do
+            expect { perform }.to(
+              raise_error(
+                ApplicationService::Exceptions::Input,
+                "[Usual::DynamicOptions::Format::Date::Optional::Example1] " \
+                "Input `started_on` does not match `date` format"
+              )
+            )
+          end
+        end
+      end
+    end
+
+    context "when the input arguments are invalid" do
+      it { expect { perform }.to have_input(:started_on).valid_with(attributes).type(String).optional }
+    end
+  end
+
+  describe ".call" do
+    subject(:perform) { described_class.call(**attributes) }
+
+    let(:attributes) do
+      {
+        started_on: started_on
+      }
+    end
+
+    let(:started_on) { nil }
+
+    include_examples "check class info",
+                     inputs: %i[started_on],
+                     internals: %i[],
+                     outputs: %i[started_on]
+
+    context "when the input arguments are valid" do
+      describe "and the data required for work is also valid" do
+        it { expect(perform).to have_output(:started_on?).with(false) }
+        it { expect(perform).to have_output(:started_on).with(nil) }
+      end
+
+      describe "but the data required for work is invalid" do
+        describe "because the format is not suitable for `date`" do
+          let(:started_on) { "2023-14-14" }
+
+          it "returns expected error" do
+            expect { perform }.to(
+              raise_error(
+                ApplicationService::Exceptions::Input,
+                "[Usual::DynamicOptions::Format::Date::Optional::Example1] " \
+                  "Input `started_on` does not match `date` format"
+              )
+            )
+          end
+        end
+      end
+    end
+
+    context "when the input arguments are invalid" do
+      it { expect { perform }.to have_input(:started_on).valid_with(attributes).type(String).optional }
+    end
+  end
+end
