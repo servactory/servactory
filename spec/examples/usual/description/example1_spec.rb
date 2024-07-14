cf533476dbfa7190fdc98399c92ebe7c7115100f
Anton <profox.rus@gmail.com>
2024-07-14 14:13:40 +0900

    Add tests for valid and invalid input cases
    The changes introduce two new test contexts in the example1_spec.rb file. One context checks if the function properly handles valid inputs, by successfully calling the class with preset attributes. The more significant addition is the test for invalid inputs, which is expected to result in an exception with a specific note.

--- spec/examples/usual/description/example1_spec.rb
+++ spec/examples/usual/description/example1_spec.rb
@@ -35,4 +35,39 @@ RSpec.describe Usual::Description::Example1, type: :service do
       end
     end
   end
+
+  describe ".call" do
+    subject(:perform) { described_class.call(**attributes) }
+
+    let(:attributes) do
+      {
+        id:
+      }
+    end
+
+    let(:id) { "61d74de0-7b70-4f80-aefd-d86005d859ed" }
+
+    include_examples "check class info",
+                     inputs: %i[id],
+                     internals: %i[],
+                     outputs: %i[]
+
+    context "when the input arguments are valid" do
+      describe "and the data required for work is also valid" do
+        include_examples "success result class"
+      end
+    end
+
+    context "when the input arguments are invalid" do
+      it do
+        expect { perform }.to(
+          have_input(:id)
+            .valid_with(attributes)
+            .type(String)
+            .required
+            .note("Payment identifier in an external system")
+        )
+      end
+    end
+  end
 end
