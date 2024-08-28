1a3e519ad436e2717435ae4b1f29836006d1d82e
Anton <profox.rus@gmail.com>
2024-08-29 00:23:11 +0700

    Add Example1 and Example2 services with tests
    Implemented two service classes, `Example1` and `Example2`, under the Usual::ToH module. Each service constructs a full name from provided first, middle, and last names, handling optional middle names gracefully. Comprehensive RSpec tests ensure correct behavior and validate inputs and outputs.

---
+++ spec/examples/usual/to_h/example2_spec.rb
@@ -0,0 +1,109 @@
+# frozen_string_literal: true
+
+RSpec.describe Usual::ToH::Example2, type: :service do
+  describe ".call!" do
+    subject(:perform) { described_class.call!(**attributes) }
+
+    let(:attributes) do
+      {
+        first_name:,
+        middle_name:,
+        last_name:
+      }
+    end
+
+    let(:first_name) { "John" }
+    let(:middle_name) { "Fitzgerald" }
+    let(:last_name) { "Kennedy" }
+
+    include_examples "check class info",
+                     inputs: %i[first_name middle_name last_name],
+                     internals: %i[],
+                     outputs: %i[full_name]
+
+    context "when the input arguments are valid" do
+      describe "and the data required for work is also valid" do
+        include_examples "success result class"
+
+        it { expect(perform).to have_output(:full_name).with("John Fitzgerald Kennedy") }
+
+        it "returns the result as a hash" do
+          expect(perform.to_h).to match(
+            full_name: "John Fitzgerald Kennedy"
+          )
+        end
+
+        describe "even if `middle_name` is not specified" do
+          let(:middle_name) { nil }
+
+          it { expect(perform).to have_output(:full_name).with("John Kennedy") }
+
+          it "returns the result as a hash" do
+            expect(perform.to_h).to match(
+              full_name: "John Kennedy"
+            )
+          end
+        end
+      end
+    end
+
+    context "when the input arguments are invalid" do
+      it { expect { perform }.to have_input(:first_name).valid_with(attributes).type(String).required }
+      it { expect { perform }.to have_input(:middle_name).valid_with(attributes).type(String).optional }
+      it { expect { perform }.to have_input(:last_name).valid_with(attributes).type(String).required }
+    end
+  end
+
+  describe ".call" do
+    subject(:perform) { described_class.call(**attributes) }
+
+    let(:attributes) do
+      {
+        first_name:,
+        middle_name:,
+        last_name:
+      }
+    end
+
+    let(:first_name) { "John" }
+    let(:middle_name) { "Fitzgerald" }
+    let(:last_name) { "Kennedy" }
+
+    include_examples "check class info",
+                     inputs: %i[first_name middle_name last_name],
+                     internals: %i[],
+                     outputs: %i[full_name]
+
+    context "when the input arguments are valid" do
+      describe "and the data required for work is also valid" do
+        include_examples "success result class"
+
+        it { expect(perform).to have_output(:full_name).with("John Fitzgerald Kennedy") }
+
+        it "returns the result as a hash" do
+          expect(perform.to_h).to match(
+            full_name: "John Fitzgerald Kennedy"
+          )
+        end
+
+        describe "even if `middle_name` is not specified" do
+          let(:middle_name) { nil }
+
+          it { expect(perform).to have_output(:full_name).with("John Kennedy") }
+
+          it "returns the result as a hash" do
+            expect(perform.to_h).to match(
+              full_name: "John Kennedy"
+            )
+          end
+        end
+      end
+    end
+
+    context "when the input arguments are invalid" do
+      it { expect { perform }.to have_input(:first_name).valid_with(attributes).type(String).required }
+      it { expect { perform }.to have_input(:middle_name).valid_with(attributes).type(String).optional }
+      it { expect { perform }.to have_input(:last_name).valid_with(attributes).type(String).required }
+    end
+  end
+end
