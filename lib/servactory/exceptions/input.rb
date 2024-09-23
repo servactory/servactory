20f5c4e82172e759972287919c3dfcc3311fcb9f
Anton <profox.rus@gmail.com>
2024-09-24 00:32:19 +0700

    Add access to context in exceptions for use inside Datory (#243)

--- lib/servactory/exceptions/input.rb
+++ lib/servactory/exceptions/input.rb
@@ -9,6 +9,7 @@ module Servactory
                   :meta
 
       def initialize(context:, message:, input_name: nil, meta: nil)
+        @context = context
         @service = context.send(:servactory_service_info)
         @message = message
         @input_name = input_name&.to_sym
@@ -16,6 +17,11 @@ module Servactory
 
         super(message)
       end
+
+      private
+
+      # API: Datory
+      attr_reader :context
     end
   end
 end
