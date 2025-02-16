b3594a17faba774016935c3a062c2e213c4859af
Anton <profox.rus@gmail.com>
2025-02-16 16:11:53 +0700

    Refactor input, internal, and output option builders
    Extract shared logic to improve code reuse and readability. Introduced `enrich_options_for` and `build_option_from` methods to streamline option processing. Removed inline method length suppression, adhering to cleaner coding standards.

--- lib/servactory/info/builder.rb
+++ lib/servactory/info/builder.rb
@@ -23,20 +23,13 @@ module Servactory
 
       private
 
-      def build_inputs_with(collection_of_inputs:, dynamic_options:) # rubocop:disable Metrics/MethodLength
+      def build_inputs_with(collection_of_inputs:, dynamic_options:)
         @inputs = collection_of_inputs.to_h do |input|
           options = build_options_for(input, dynamic_options)
+          options = enrich_options_for(input, options)
 
-          actor = input.class::Actor.new(input)
-          must = input.collection_of_options.find_by(name: :must)
-
-          options.merge!(
-            actor:,
-            types: input.types,
-            required: input.required,
-            default: input.default,
-            must: must.body
-          )
+          options[:required] = input.required
+          options[:default] = input.default
 
           [
             input.name,
@@ -45,18 +38,10 @@ module Servactory
         end
       end
 
-      def build_internals_with(collection_of_internals:, dynamic_options:) # rubocop:disable Metrics/MethodLength
+      def build_internals_with(collection_of_internals:, dynamic_options:)
         @internals = collection_of_internals.to_h do |internal|
           options = build_options_for(internal, dynamic_options)
-
-          actor = internal.class::Actor.new(internal)
-          must = internal.collection_of_options.find_by(name: :must)
-
-          options.merge!(
-            actor:,
-            types: internal.types,
-            must: must.body
-          )
+          options = enrich_options_for(internal, options)
 
           [
             internal.name,
@@ -65,18 +50,10 @@ module Servactory
         end
       end
 
-      def build_outputs_with(collection_of_outputs:, dynamic_options:) # rubocop:disable Metrics/MethodLength
+      def build_outputs_with(collection_of_outputs:, dynamic_options:)
         @outputs = collection_of_outputs.to_h do |output|
           options = build_options_for(output, dynamic_options)
-
-          actor = output.class::Actor.new(output)
-          must = output.collection_of_options.find_by(name: :must)
-
-          options.merge!(
-            actor:,
-            types: output.types,
-            must: must.body
-          )
+          options = enrich_options_for(output, options)
 
           [
             output.name,
@@ -87,24 +64,39 @@ module Servactory
 
       ##########################################################################
 
-      def build_options_for(argument, dynamic_options) # rubocop:disable Metrics/MethodLength
-        argument.options.to_h do |key, value|
+      def build_options_for(attribute, dynamic_options)
+        attribute.options.to_h do |key, value|
           dynamic_option = dynamic_options.find_by(name: key)
 
           next [nil, nil] if dynamic_option.nil?
 
           body_key = dynamic_option.meta.fetch(:body_key)
 
-          option = {
-            body_key => value.is_a?(Hash) ? value.fetch(body_key, value) : value,
-            message: value.is_a?(Hash) ? value.fetch(:message, nil) : nil
-          }
+          option = build_option_from(body_key:, value:)
 
           [
             dynamic_option.name,
             option
           ]
         end.compact
+      end
+
+      def build_option_from(body_key:, value:)
+        {
+          body_key => value.is_a?(Hash) ? value.fetch(body_key, value) : value,
+          message: value.is_a?(Hash) ? value.fetch(:message, nil) : nil
+        }
+      end
+
+      def enrich_options_for(attribute, options)
+        actor = attribute.class::Actor.new(attribute)
+        must = attribute.collection_of_options.find_by(name: :must)
+
+        options.merge(
+          actor:,
+          types: attribute.types,
+          must: must.body
+        )
       end
     end
   end
