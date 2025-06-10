1eb7ed4147c9ed730291104705a6d50eb568f65f
Anton Sokolov <profox.rus@gmail.com>
2025-06-10 20:25:40 +0700

    Add new example and spec for Wrong::Basic::Example13 service

---
+++ examples/wrong/basic/example13.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Wrong
+  module Basic
+    class Example13 < ApplicationService::Base
+      input :invoice_number, type: String
+
+      internal :prepared_invoice_number, type: String
+
+      output :invoice_number, type: Integer
+
+      make :assign_invoice_number
+
+      private
+
+      def assign_invoice_number
+        outputs.invoice_number = internals.prepared_invoice_number
+      end
+    end
+  end
+end
