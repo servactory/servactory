4eee1bd5513f267e19ff5545057e5378ad6c3854
Anton Sokolov <profox.rus@gmail.com>
2025-06-22 05:18:48 +0700

    Refactor: simplify config setup and remove `reset_config`
    - Removed unused `reset_config` method from `DSL::ClassMethods`.
    - Simplified configuration logic by eliminating redundant reset mechanism.
    - Ensured that `@config` initialization remains encapsulated within `config` method.

--- lib/servactory/configuration/dsl.rb
+++ lib/servactory/configuration/dsl.rb
@@ -10,21 +10,11 @@ module Servactory
       end
 
       module ClassMethods
-        def inherited(child)
-          super
-
-          child.send(:reset_config)
-        end
-
         def config
           @config ||= Servactory::Configuration::Setup.build_for(self)
         end
 
         private
-
-        def reset_config
-          @config = nil
-        end
 
         def configuration(&block)
           self.configuration_block = block
