b4e36957cd794d45f19577359522a1db8328e832
Anton Sokolov <profox.rus@gmail.com>
2025-06-23 04:19:32 +0700

    Add `Example24` for reserved output name validation
    - Created `Example24` under `Wrong::Arguments` to test the reserved output name `error`.
    - Added RSpec tests to verify exceptions are raised when the reserved name `error` is used as an output attribute.
    - Ensured alignment of validation logic and consistency in error messages across `.call` and `.call!` methods.

---
+++ examples/wrong/arguments/example24.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Wrong
+  module Arguments
+    class Example24 < ApplicationService::Base
+      output :error, type: String
+
+      make :assign_error
+
+      private
+
+      def assign_error
+        outputs.error = "test"
+      end
+    end
+  end
+end 
