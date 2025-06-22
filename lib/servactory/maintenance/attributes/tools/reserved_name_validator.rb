e3c887bd7d0a36e06d56b45860f89d62a2e9e0e8
Anton Sokolov <profox.rus@gmail.com>
2025-06-22 23:43:16 +0700

    Refactor ReservedNameValidator to enhance maintainability
    Changed `ReservedNameValidator` from a module to a class to improve encapsulation and simplify method organization. Introduced instance methods and memoization for `name` and `type` attributes. Extracted private helper methods for better readability and logic separation during reserved name validation.

--- lib/servactory/maintenance/attributes/tools/reserved_name_validator.rb
+++ lib/servactory/maintenance/attributes/tools/reserved_name_validator.rb
@@ -4,7 +4,7 @@ module Servactory
   module Maintenance
     module Attributes
       module Tools
-        module ReservedNameValidator
+        class ReservedNameValidator
           RESERVED = {
             input: %i[fail success],
             output: %i[failure success],
@@ -12,28 +12,56 @@ module Servactory
           }.freeze
 
           def self.validate!(context:, attribute:)
-            name = attribute.respond_to?(:name) ? attribute.name.to_sym : nil
-            type =
-              if attribute.respond_to?(:input?) && attribute.input?
-                :input
-              elsif attribute.respond_to?(:output?) && attribute.output?
-                :output
-              elsif attribute.respond_to?(:internal?) && attribute.internal?
-                :internal
-              end
-            return unless name && type && RESERVED[type]&.include?(name)
+            new(context: context, attribute: attribute).validate!
+          end
 
-            exception_class = case type
-                             when :input then Servactory::Exceptions::Input
-                             when :output then Servactory::Exceptions::Output
-                             when :internal then Servactory::Exceptions::Internal
-                             end
+          def initialize(context:, attribute:)
+            @context = context
+            @attribute = attribute
+          end
 
+          def validate!
+            return unless reserved_name?
+            raise_exception!
+          end
+
+          private
+
+          attr_reader :context, :attribute
+
+          def name
+            attribute.respond_to?(:name) ? attribute.name.to_sym : nil
+          end
+
+          def type
+            return :input if attribute.respond_to?(:input?) && attribute.input?
+            return :output if attribute.respond_to?(:output?) && attribute.output?
+            return :internal if attribute.respond_to?(:internal?) && attribute.internal?
+            nil
+          end
+
+          def reserved_name?
+            name && type && RESERVED[type]&.include?(name)
+          end
+
+          def exception_class
+            case type
+            when :input then Servactory::Exceptions::Input
+            when :output then Servactory::Exceptions::Output
+            when :internal then Servactory::Exceptions::Internal
+            end
+          end
+
+          def raise_exception!
             raise exception_class.new(
               context: context,
-              message: "[#{context.class.name}] Имя :#{name} зарезервировано для #{type}s",
+              message: exception_message,
               "#{type}_name": name
             )
+          end
+
+          def exception_message
+            "[#{context.class.name}] Имя :#{name} зарезервировано для #{type}s"
           end
         end
       end
