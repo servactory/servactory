4185e77d33ccaae5d492fd9a0328d90928eefd81
Anton Sokolov <profox.rus@gmail.com>
2025-06-25 03:03:17 +0700

    Update locales

--- spec/examples/usual/dynamic_options/schema/example_collection_invalid_spec.rb
+++ spec/examples/usual/dynamic_options/schema/example_collection_invalid_spec.rb
@@ -14,15 +14,12 @@ RSpec.describe Usual::DynamicOptions::Schema::ExampleCollectionInvalid, type: :s
     end
 
     it "raises an error for invalid element in collection" do
-      expect { perform }.to raise_error(ApplicationService::Exceptions::Input, "[Usual::DynamicOptions::Schema::ExampleCollectionInvalid] Wrong element in input collection `items`: hash element for `name` has NilClass instead of String")
-    end
-
-    it "returns an error for invalid collection element" do
-      result = described_class.call(items: [{ id: "not-an-integer", name: "Test" }])
-      expect(result).to be_failure
-      expect(result.errors[:items]).to eq([
-        "[Usual::DynamicOptions::Schema::ExampleCollectionInvalid] Wrong element in input collection `items`: hash element for `id` has String instead of Integer"
-      ])
+      expect { perform }.to(
+        raise_error(
+          ApplicationService::Exceptions::Input,
+          "[Usual::DynamicOptions::Schema::ExampleCollectionInvalid] Wrong element in input collection `items`: hash element for `name` has `NilClass` instead of `String`"
+        )
+      )
     end
   end
 end 
