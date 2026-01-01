ef48aeb10126e41608d783479b7278c045594248
Anton Sokolov <profox.rus@gmail.com>
2026-01-01 18:35:43 +0700

    Streamline hook spec tests for clarity and conciseness
    - Replaced `each` with `map` in hook yield test to simplify logic and improve readability.
    - Refactored hook type assertion to use `all` matcher for a more concise and expressive validation.
    - Removed redundant code for setting up assertions, reducing unnecessary complexity.

--- spec/servactory/stroma/hooks_spec.rb
+++ spec/servactory/stroma/hooks_spec.rb
@@ -103,15 +103,12 @@ RSpec.describe Servactory::Stroma::Hooks do
     end
 
     it "yields each hook" do
-      yielded = []
-      hooks.each { |hook| yielded << hook }
+      yielded = hooks.map { |hook| hook }
       expect(yielded.size).to eq(2)
     end
 
     it "yields Hook objects" do
-      hooks.each do |hook|
-        expect(hook).to be_a(Servactory::Stroma::Hook)
-      end
+      expect(hooks).to all(be_a(Servactory::Stroma::Hook))
     end
   end
 end
